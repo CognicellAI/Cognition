71c5097e292293a751273a9a2375c25f10694c10
Herman Haggerty <dubh3124@gmail.com>
2026-02-11 21:21:42 -0600

    Reset project for clean architecture rewrite

--- client/tui/screens/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Screens for Cognition TUI."""
-
-from client.tui.screens.project_picker import ProjectPickerScreen
-from client.tui.screens.session import SessionScreen
-
-__all__ = ["ProjectPickerScreen", "SessionScreen"]
--- client/tui/widgets/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Custom widgets for Cognition TUI."""
-
-from client.tui.widgets.chat_message import ChatMessage
-from client.tui.widgets.prompt import PromptInput
-from client.tui.widgets.status_bar import StatusBar
-from client.tui.widgets.tool_block import ToolBlock
-
-__all__ = ["ChatMessage", "PromptInput", "StatusBar", "ToolBlock"]
